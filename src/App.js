6ede175c47ac5abe148b9549378218fb950137e5
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-09-06 17:45:51 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Khayot");
+
+  const handleClick = () => {
+    setName("Khayotbek");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello my name is {name}</h2>
+      <button className="btn" onClick={handleClick}>
+        change name
+      </button>
     </div>
   );
 }
